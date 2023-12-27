eeb42b1de0857eabd6fe0a15707676113a46d5d3
Faisal <hayat.khanum92@gmail.com>
2023-12-28 00:08:35 +0530

    code added

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,23 +1,34 @@
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Main {
     public static void main(String[] args) {
-        // You can use print statements as follows for debugging, they'll be visible when running tests.
+        // You can use print statements as follows for debugging, they'll be visible
+        // when running tests.
         System.out.println("Logs from your program will appear here!");
-
-        // Uncomment this block to pass the first stage
-        //
         ServerSocket serverSocket = null;
         Socket clientSocket = null;
-
         try {
             serverSocket = new ServerSocket(4221);
             serverSocket.setReuseAddress(true);
             clientSocket = serverSocket.accept(); // Wait for connection from client.
             System.out.println("accepted new connection");
-            clientSocket.getOutputStream().write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+            PrintWriter printWriter = new PrintWriter(clientSocket.getOutputStream());
+            printWriter.println("HTTP/1.1 200 OK\r\n\r\n");
+            InputStream inputStream = clientSocket.getInputStream();
+            String requestReader =
+                    new BufferedReader(new InputStreamReader(inputStream)).readLine();
+            System.out.println("Request content: " + requestReader);
+            if (requestReader.contains("GET / HTTP/1.1")) {
+                printWriter.println("HTTP/1.1 200 OK\r\n\r\n");
+            } else {
+                printWriter.println("HTTP/1.1 404 Not Found\r\n\r\n");
+            }
+            printWriter.flush();
+            printWriter.close();
         } catch (IOException e) {
             System.out.println("IOException: " + e.getMessage());
         }
